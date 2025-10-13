57ffb669fd53a713d35f0732c9dab29f57a5ebd4
phevab <phevab1@gmail.com>
2025-10-13 15:50:54 +0000

    filter-updates

--- fees/admin.py
+++ fees/admin.py
@@ -21,7 +21,7 @@ class FeeStructureAdmin(admin.ModelAdmin):
 @admin.register(StudentFeeRecord)
 class StudentFeeRecordAdmin(admin.ModelAdmin):
     list_display = ("student", "fee_structure", "amount_paid", "balance", "is_fully_paid")
-    list_filter = ("fee_structure__academic_year", "fee_structure__term", "is_fully_paid")
+    list_filter = ("fee_structure__academic_year", "fee_structure__term", "is_fully_paid", "fee_structure")
     search_fields = ("student__user__full_name", "fee_structure__grade_class__name")
     ordering = ("student",)
     readonly_fields = ("balance", "is_fully_paid")
