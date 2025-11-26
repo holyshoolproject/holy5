a7bd7a56e93753f94d921ffd7931548a4cf0270d
phevab <phevab1@gmail.com>
2025-11-26 21:14:28 +0000

    anywherepy

--- A_sms/settings.py
+++ A_sms/settings.py
@@ -109,11 +109,23 @@ WSGI_APPLICATION = 'A_sms.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 
 
 
+
+
+
+
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -129,16 +141,6 @@ DATABASES = {
     }
 }
 
-
-
-
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
-}
 
 DATABASES = {
     'default': {
